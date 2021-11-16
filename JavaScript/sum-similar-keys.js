4bfa5b6d1be5dbf873ad276a947bb22c3e68fee8
FIRAT <frtdkmn@gmail.com>
2021-11-16 04:06:24 +0300

    Create sum-similar-keys.js

---
+++ JavaScript/sum-similar-keys.js
@@ -0,0 +1,25 @@
+var firstObj = [
+    { 'unit': 'U1', 'price': 50 },
+    { 'unit': 'U1', 'price': 50 },
+    { 'unit': 'U2', 'price': 20 },
+    { 'unit': 'U3', 'price': 40 },
+    { 'unit': 'U4', 'price': 40 }
+];
+
+var holder = {};
+
+firstObj.forEach(function(d) {
+  if (holder.hasOwnProperty(d.unit)) {
+    holder[d.unit] = holder[d.unit] + d.price;
+  } else {
+    holder[d.unit] = d.price;
+  }
+});
+
+var lastObj = [];
+
+for (var prop in holder) {
+  lastObj.push({ unit: prop, price: holder[prop] });
+}
+
+console.log(lastObj);
